daf2f0cf78d9d4b986641bc6deedc6464b51be7f
Dooley <nsewolo@gmail.com>
2014-11-26 21:59:58 -0500

    Using strict mode

--- api/api.js
+++ api/api.js
@@ -1,30 +1,33 @@
-var express = require('express'),
-    parser = require('../controller/json_loader');
+(function () {
+    'use strict';
 
-var api = express();
+    var express = require('express'),
+        parser = require('../controller/json_loader'),
+        api = express();
 
-// Get features
-//GET /api/features
-api.get('/features', function (req, res) {
-    res.send(
-        [{"features": 'features'}]
-    );
-});
+    // Get features
+    //GET /api/features
+    api.get('/features', function (req, res) {
+        res.send(
+            [{"features": 'features'}]
+        );
+    });
 
-// Get a feature
-//GET /api/features/idFeature
-api.get('/features/:id', function (req, res) {
-    res.send(
-        [{'idFeature': '/features/:id'}]
-    );
-});
+    // Get a feature
+    //GET /api/features/idFeature
+    api.get('/features/:id', function (req, res) {
+        res.send(
+            [{'idFeature': '/features/:id'}]
+        );
+    });
 
-// Get scenario
-// GET /api/features/featureId/idScenario
-api.get('/features/:id/:idScenario', function (req, res) {
-    res.send(
-        [{'scenario': 'id/idScenario'}]
-    );
-});
+    // Get scenario
+    // GET /api/features/featureId/idScenario
+    api.get('/features/:id/:idScenario', function (req, res) {
+        res.send(
+            [{'scenario': 'id/idScenario'}]
+        );
+    });
 
-module.exports = api;
+    module.exports = api;
+}());
